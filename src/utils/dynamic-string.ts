c45fb93478b838cebccc2de94606f3e152891b8f
pmqueiroz <pedroqueiroz593@gmail.com>
2021-08-18 01:03:50 -0300

    debug: see dyanmic template string

--- src/utils/dynamic-string.ts
+++ src/utils/dynamic-string.ts
@@ -1,10 +1,13 @@
 import * as R from 'ramda'
+import * as Core from '@actions/core'
 
 /**
  *  @reference this code below come from a module created to react that will not work on node
  * 
  * {@link https://github.com/rodrigo-picanco/dynamic-string}
 */
-export const dynamicTemplate = R.curry((templateString: string, templateVariables: Record<string, any>) =>
-  templateString.replace(/\${(.*?)}/g, (_, g) => templateVariables[g])
-)
+export const dynamicTemplate = R.curry((templateString: string, templateVariables: Record<string, any>) =>{
+   Core.debug(JSON.stringify({templateString}))
+
+   return templateString.replace(/\${(.*?)}/g, (_, g) => templateVariables[g])
+})
